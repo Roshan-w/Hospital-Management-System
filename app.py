ab4390d69a96db49778201b9f9d07814f3e1ab82
Roshan Bhanudas Warade <roshanwarade2004@gmail.com>
2026-04-28 10:39:23 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from application.__init__ import create_app
 
 app, celery = create_app()
@@ -8,5 +9,6 @@ def catch_all(path):
     from flask import render_template
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
